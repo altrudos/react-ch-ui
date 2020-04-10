1d1338ab80937b989def351b720a71bd0eb67b75
Bryan Cua <bryan@luxe.technology>
2020-04-10 14:31:37 -0700

    charity-selector-fix

--- modules/charities/charity-selector.tsx
+++ modules/charities/charity-selector.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {AsyncState} from "components/async"
 import {Charity} from "model/charity";
 import CharityApi from "api/charities";
 import Select from "components/form/select"
+import useAsync from "../../hooks/use-async"
 
 export type CharitySelectorProps = {
     className?: string
@@ -22,42 +23,38 @@ export default function CharitySelector(
         setValue,
     } : CharitySelectorProps
 ) {
-    const [state, setState] = useState<AsyncState>(AsyncState.Available)
-    const [charities, setCharities] = useState<Charity[]>(null)
 
-    if (state == AsyncState.Available) {
-        setState(AsyncState.Loading)
-        try {
-            /*const resp =  CharityApi.all()
-            setCharities(resp.Data)
-            setState(AsyncState.Success)*/
-        } catch (ex) {
-            setState(AsyncState.Error)
-            throw ex
-        }
+    const {
+        status,
+        runAsync,
+    } = useAsync(AsyncState.Available)
+
+    const [charities, setCharities] = useState<Charity[]>([])
+
+    useEffect(()=>{
+        runAsync(loadCharities, {
+            AvailableMsg: '',
+            ThrowError: false,
+            AvailableAfterError: true
+        })
+
+        //TODO: we may want to change this to run each time
+        // on search, for example.
+    }, [])
+
+    async function loadCharities() {
+        const charities = await CharityApi.all()
+        setValue(charities.Data[0].Id)
+        setCharities(charities.Data)
     }
 
-    let options = [{
+    let options = status == AsyncState.Available ? charities.map(c => ({
+        value: c.Id,
+        label: c.Name,
+    })) : [{
         value: '627e0410-c75d-48c8-b41f-6318d04f1e65',
-        label: 'loadingy'
+        label: 'loading'
     }]
-    if (state == AsyncState.Success) {
-        options = charities.map(c =>{
-            return {
-                value: c.Id,
-                label: c.Name,
-            }
-        })
-    } else if (state == AsyncState.Available) {
-        CharityApi.all().then((c) => {
-            setCharities(c.Data)
-            setState(AsyncState.Success)
-            setValue(c.Data[0].Id)
-        }).catch((ex) => {
-            setState(AsyncState.Error)
-            console.log('ex', ex)
-        })
-    }
 
     return <Select name={name} setValue={setValue} value={value} options={options} />
 }
